76f45721ffe03a3ba588e57486ddc008ebd84012
Wan Muzaffar <wanmuz86@gmail.com>
2025-12-03 10:40:04 +0800

    updated code with component

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+
 import App from './App.tsx'
+
+// Create a new element, at document.getElementId('root') -> The div inside index.htm;
+// Replace/render with App component
+// <App/> componetn is defined in ./App.tsx
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
